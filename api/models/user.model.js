925b74e1f4b94d2120cd1ae1e358416a1293ca28
sachin4real <isurusachin123@gmail.com>
2024-07-10 03:24:37 +0530

    create signup routes

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -5,14 +5,21 @@ const userSchema = new mongoose.Schema({
         type: String,
         required:true,
         unique:true,
+      
 
+    },
+  
+    email: {
+        type: String,
+        required:true,
+        unique:true,
     },
 
     password: {
         type: String,
         required:true,
 
-    },
+    }
 },{timestamps:true}
 
 );
