249b7badddaa135b6b8228fa972bb28d79b3a8df
sachin4real <isurusachin123@gmail.com>
2024-07-11 02:25:48 +0530

    create footer componenet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
@@ -21,7 +22,7 @@ export default function App() {
         <Route path="/projects" element={<Projects />} />
         
       </Routes>
-   
+      <Footer/>
    </BrowserRouter>
   )
 }
